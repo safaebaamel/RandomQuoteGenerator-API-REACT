233f2a72658b597c1c47850b4889b271120c6ac8
Sapae Safae <sapaebaamel@gmail.com>
2021-06-10 15:57:46 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -30,6 +30,11 @@ class App extends React.Component {
             <div className="app">
                 <div className="card">
                     <h1 className="heading">{advice}</h1>
+                    <button className="button" onClick={this.fetchAdvice}>
+                        <span>
+                            Give Me Advice!
+                        </span>
+                    </button>
                 </div>
             </div>
         );
